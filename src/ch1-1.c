63091e6c5554e28e129c8c58dbeb0f8a6ea9349a
Jay Flaherty <jayflaherty@gmail.com>
2021-09-09 09:41:08 -0500

    Fix missing ch6 subsection and TOC entry
    I think there was an issue with the pandoc app converting html to LaTex for chapter 6.
    Specifically with the missing subsection tag. This in turn caused the TOC to get generated
    without chapter 6 referenced. This has been corrected as well as some cleanup to the
    TOC file (delete  subsubsections) as well as taking the center tag out of pointers.htm

--- src/ch1-1.c
+++ src/ch1-1.c
@@ -9,9 +9,9 @@ int main(void)
     k = 2;
     ptr = &k;
     printf("\n");
-    printf("j has the value %d and is stored at %p\n", j, (void *)&j);
-    printf("k has the value %d and is stored at %p\n", k, (void *)&k);
-    printf("ptr has the value %p and is stored at %p\n", (void*)ptr, (void *)&ptr);
+    printf("j has the size %lu, value %d, and is stored at %p\n", sizeof(j), j, (void *)&j);
+    printf("k has the size %lu, value %d, and is stored at %p\n", sizeof(k), k, (void *)&k);
+    printf("ptr has the size %lu, value %p, and is stored at %p\n", sizeof(ptr), (void *)ptr, (void *)&ptr);
     printf("The value of the integer pointed to by ptr is %d\n", *ptr);
 
     return 0;
